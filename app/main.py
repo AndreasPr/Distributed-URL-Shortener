8c5daa70d7808670d5739bfa053ca28884a2b304
Andreas Priftis <andreaspriftisdeveloper@gmail.com>
2026-05-19 13:46:04 -0500

    updated main.py by adding vercel domain in CORS

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
         "http://localhost:3001",
         "http://localhost:3002",
         "http://localhost:3003",
+        "https://distributed-url-shortener-two.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
